7907963870fd99bfaad8f8c57bcac111f351dd1b
Simon Nagl <sin6332@thi.de>
2016-04-13 19:34:16 +0200

    Erste Version: View und Controller MediVerwaltung

--- www/js/mediVerwaltungCtrl.js
+++ www/js/mediVerwaltungCtrl.js
@@ -1,7 +1,26 @@
 angular.module('starter.mediVerwaltungCtrl', [])
 
-.controller('mediVerwaltungCtrl', function($scope, $ionicModal, $timeout) {
+.controller('mediVerwaltungCtrl', function($scope) {
+  
+    $scope.mediData = [
+        { 
+            name : "Penicilin",
+            dose : "200mg",
+            pack_gr : 20
 
-    //Controller mediVerwaltungCtrl
+        },
+        {
+            name : "Aspirin",
+            dose : "200mg",
+            pack_gr : 50
+        } 
+  ];
 
+
+  // Eingabe mit Popup
+  // Layout keine Präferenz
+
+  $scope.edit_medi = function(mediName) {
+      $scope.mediData.push( mediName );
+  }
 });
