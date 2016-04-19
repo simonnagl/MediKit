92c12c610430778ebf97b7d1a649a8369c75342e
Johann-Peter Veith <Johann-Peter Veith>
2016-04-19 13:21:16 +0200

    Anzeige nicht genommener Medis im Controller implementiert;

--- www/js/startseiteCtrl.js
+++ www/js/startseiteCtrl.js
@@ -49,7 +49,41 @@ angular.module('starter.startseiteCtrl', [])
          console.log('Tapped!', res);
       });    
    };
+   
 ///Pupup ende
 
+////Liste im Popup
 
+            $scope.toggleItem = function(item){
+                item.checked = !item.checked;
+            };
+            $scope.itemButtons = [
+                {
+                text: 'Edit',
+                type: 'button-assertive',
+                onTap: function(item) {
+                    alert('Edit Item: ' + item.id);
+                }
+                }/*,
+                {
+                text: 'Share',
+                type: 'button-calm',
+                onTap: function(item) {
+                    alert('Share Item: ' + item.id);
+                }
+                }*/
+            ];
+            
+            $scope.onItemDelete = function(item) {
+                $scope.items.splice($scope.items.indexOf(item), 1);
+            };
+            
+            $scope.items = [
+                { id: 1 },
+                { id: 2 },
+                {id: 3}
+            ];
+            //// Infos https://codepen.io/ionic/pen/hqcju
+            //// http://codepen.io/calendee/pen/Enmaw
+////Ende Liste im Popup
 });
