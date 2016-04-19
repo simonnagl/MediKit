70ab7653bd0ed927a1f1fa18f2bc84ffba2d577d
Anna Leitmann <anl2083@thi.de>
2016-04-19 12:07:31 +0200

    statische Historyview

--- www/js/historieCtrl.js
+++ www/js/historieCtrl.js
@@ -1,27 +1,31 @@
-angular.module('starter.historieCtrl', ['jkuri.datepicker'])
+angular.module('starter.historieCtrl', [])
 
 .controller('HistorieCtrl', function($scope, $ionicModal, $timeout) {
 
     //Controller historieCtrl
-    $ionicModal.fromTemplateUrl('templates/datemodal.html', 
-        function(modal) {
-            $scope.datemodal = modal;
-        },
-        {
+    
+    $scope.von = new Date();
+    $scope.bis = new Date();
+    $scope.medicine = ["Aspirin 200mg 19.4.2016 8:00", "Paracetamol 200mg 19.4.2016 12:00", "Antibiotika 100mg 14:00"];
+  //  $ionicModal.fromTemplateUrl('templates/datemodal.html', 
+  //      function(modal) {
+  //          $scope.datemodal = modal;
+  //      },
+  //      {
         // Use our scope for the scope of the modal to keep it simple
-        scope: $scope, 
+  //      scope: $scope, 
         // The animation we want to use for the modal entrance
-        animation: 'slide-in-up'
-        }
-    );
+  //      animation: 'slide-in-up'
+  //      }
+  //  );
     
-    $scope.opendateModal = function() {
-      $scope.datemodal.show();
-    };
+  //  $scope.opendateModal = function() {
+  //    $scope.datemodal.show();
+  //  };
     
-    $scope.closedateModal = function(modal) {
-      $scope.datemodal.hide();
-      $scope.datepicker = modal;
-    };
+  //  $scope.closedateModal = function(modal) {
+  //    $scope.datemodal.hide();
+  //    $scope.datepicker = modal;
+  //  };
     
 });
