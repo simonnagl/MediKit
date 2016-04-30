946388e3558f00eef3d7a3541fdee5b4eee2e24b
sef8754 <sef8754@thi.de>
2016-04-30 11:57:32 +0200

    - Codeteile um Einnahmeobjekt optimiert und bugfixed durch Fehlerabfangung - Die Einnahmezeit richtig verarbeiten und speichern - Medieinnahmen: Nach dem Laden einer Einnahme werden die Werte jetzt richtig in beiden Views anzeigt - Begonnen mit Validierung (in der View und Ctrl)

--- www/js/mediEinnahmeCtrl.js
+++ www/js/mediEinnahmeCtrl.js
@@ -1,5 +1,6 @@
 angular.module('starter.mediEinnahmeCtrl', [])
 
+
 .controller('mediEinnahmeCtrl', ['$window','$log', '$scope', '$ionicModal', '$timeout', '$ionicPopup', 'MediStorage', function($window, $log, $scope, $ionicModal, $timeout, $ionicPopup, MediStorage) {
 
     //Controller mediEinnahmeCtrl
@@ -15,6 +16,28 @@ angular.module('starter.mediEinnahmeCtrl', [])
   // Form data for the mediEinnahme_neu modal
   $scope.mediEinnahmeData = [];
   $scope.mediData = [];
+  $scope.checkvalid = {};
+  
+  default_einnahme = {
+        mediname : "",
+        einnahmemenge : {
+            menge : undefined,
+            einheit : undefined
+        },
+        uhrzeit : new Date(new Date().toString().slice(0,21)),
+        wiederholungstag : {
+          mo : false,
+          di : false,
+          mi : false,
+          do : false,
+          fr : false,
+          sa : false,
+          so : false, 
+        },
+        wiederholungsbeginn : new Date(),
+        wiederholungsende : "",
+        vibration : false,
+    }
   
   //Alle Medikamente laden und anschließend nur die Medikamente heraussuchen die einnahmen haben
   $scope.loadMedisForEinnahme = function() {
@@ -46,13 +69,13 @@ angular.module('starter.mediEinnahmeCtrl', [])
   // Create the mediEinnahme_neu modal that we will use later
     $ionicModal.fromTemplateUrl('templates/mediEinnahme_edit.html', {
     scope: $scope
-  }).then(function(einnahme) {
-    $scope.einnahme = einnahme;
+  }).then(function(modal) {
+    $scope.einnahmeview = modal;
   });
   
   // Triggered in the mediEinnahme_neu modal to close it
   $scope.closeMediEinnahmeNeu = function() {
-    $scope.einnahme.hide();
+    $scope.einnahmeview.hide();
     
     //Resetfunktion in Billig:
         $scope.einnahme.mediname = "";
@@ -66,7 +89,7 @@ angular.module('starter.mediEinnahmeCtrl', [])
   
   // Triggered in the mediEinnahme_edit modal to close it
   $scope.closeMediEinnahmeEdit = function() {
-    $scope.einnahme.hide();
+    $scope.einnahmeview.hide();
     
     //Resetfunktion in Billig:
         $scope.einnahme.mediname = "";
@@ -124,19 +147,25 @@ angular.module('starter.mediEinnahmeCtrl', [])
   
 //// Open the mediEinnahme_neu modal
   $scope.mediEinnahmeNeu = function() {
-    $scope.isNewEin = true;
+    $scope.isNewEin = true;             //kennzeichnet neue Einnahme
     $scope.id = $scope.creatId();
-    $scope.einnahme.show();
+    $scope.einnahme = angular.copy(default_einnahme);
+    $log.debug("Uhrzeit: " + $scope.einnahme.uhrzeit);
+    $scope.einnahmeview.show();
     
-    //kennzeichne neu bzw. undefined für Delete-Fehler abfangen.
-    $scope.einnahme.index = undefined;
-    $log.info($scope.einnahme.index);
   };
   
 //// Open the mediEinnahme_edit modal
   $scope.mediEinnahmeEdit = function(editObject) {
     $scope.isNewEin = false;
       $log.info('Edit Object: ', editObject);
+        
+        //Falls $scope.einnahme aus irgendwelchen Gründen nicht vorhanden ist,
+        //um Fehler abzufangen, wird eine neue Instanz vorläufig geladen
+        if($scope.einnahme == undefined){
+          $scope.einnahme = angular.copy(default_einnahme);
+        }
+        
         
         //$scope.einnahme.med muss gesetzt werden
         $scope.setEinnahmeMed(editObject.mediname);
@@ -152,15 +181,40 @@ angular.module('starter.mediEinnahmeCtrl', [])
         $scope.einnahme.vibration = editObject.vibration;
         $log.debug("Einnahme: " + $scope.einnahme);
         
-    $scope.einnahme.show();
+    $scope.einnahmeview.show();
   };
   
-  // Perform the mediEinnahme_neu action when the user add the einnahme form
+//// Validation
+  $scope.checkInput = function () {
+    if( $scope.einnahme.med == undefined){
+      $scope.whichinvalid = "Medikament";
+      return false;
+    } else if ($scope.einnahme.einnahmemenge.menge == undefined) {
+      $scope.whichinvalid = "Einnahme Menge";
+      return false;
+    } else if ($scope.einnahme.einnahmemenge.einheit == undefined) {
+      $scope.whichinvalid = "Einheit Menge";
+      return false;
+    } else {
+      return true;
+    }
+  }  
+  
+//// Perform the mediEinnahme_neu action when the user add the einnahme form
   
    $scope.addEinnahme = function () {
+     
+     //Validation von Eingaben
+     if(!$scope.checkInput()){
+       $log.debug("Nicht Valide: " + $scope.whichinvalid);
+       return "Nicht Valide";
+     }
+     
      //einnahme.med in einen richtiges Objekt umwandeln
      $log.debug("Format von einname.med vor parse: " + $scope.einnahme.med);
-     $scope.einnahme.med = JSON.parse($scope.einnahme.med);
+     if($scope.isNewEin){
+       $scope.einnahme.med = JSON.parse($scope.einnahme.med);
+     }
      
      $log.info("addEinnahme: " + $scope.einnahme.med.mediname);
      
@@ -199,7 +253,6 @@ angular.module('starter.mediEinnahmeCtrl', [])
               MediStorage.updateMedikament($scope.mediData[i]);
             }
           }
-          
       } else {
         //Andernfalls soll die Einnahme aktuallisiert werden
 
@@ -233,13 +286,13 @@ angular.module('starter.mediEinnahmeCtrl', [])
       }//else ende
       
         //Resetfunktion in Billig:
-        $scope.einnahme.mediname = "";
+        /*$scope.einnahme.mediname = "";
         $scope.einnahme.einnahmemenge = "";
         $scope.einnahme.uhrzeit = "";
         $scope.einnahme.wiederholungstag = "";
         $scope.einnahme.wiederholungsbeginn = "";
         $scope.einnahme.wiederholungsende = "";
-        $scope.einnahme.vibration = "";
+        $scope.einnahme.vibration = "";*/
         
         //Liste neu Laden
         $scope.loadMedisForEinnahme();
