8fbb67b4073cf6d002ce3347274deae15f093c66
Anna Leitmann <anl2083@thi.de>
2016-05-12 14:01:24 +0200

    Abschluss Meilenstein 4

--- www/js/historieCtrl.js
+++ www/js/historieCtrl.js
@@ -4,14 +4,47 @@ angular.module('starter.historieCtrl', ['ionic'])
 
     //Controller historieCtrl
     
-    $scope.medicine = [{id:"123", mediname:"Aspirin", medidosis:"200mg", einnahmemenge:"2Tabletten", einnahmezeitsoll:"09.04.2016 18:00", einnahmezeitist:""},
-    {id:"1234", mediname:"Aspirin", medidosis:"200mg", einnahmemenge:"2Tabletten", einnahmezeitsoll:"10.04.2016 18:00", einnahmezeitist:""},
-    {id:"12345", mediname:"Aspirin", medidosis:"200mg", einnahmemenge:"2Tabletten", einnahmezeitsoll:"11.04.2016 18:00", einnahmezeitist:""},
-    {id:"234", mediname:"Paracetamol", medidosis:"100mg", einnahmemenge:"1Tablette", einnahmezeitsoll:"02.04.2016 12:00", einnahmezeitist:"2.4.2016 12:05"}];
+    $scope.medicine = [{id:"123", mediname:"Aspirin", medidosis:"200mg", einnahmemenge:"2Tabletten", einnahmezeitsoll:new Date(2016,04,1, 12,00,00), einnahmezeitist:""},
+    {id:"1234", mediname:"Aspirin", medidosis:"200mg", einnahmemenge:"2Tabletten", einnahmezeitsoll:new Date(2016,03,01,18,00,00), einnahmezeitist:"TEst"},
+    {id:"12345", mediname:"Aspirin", medidosis:"200mg", einnahmemenge:"2Tabletten", einnahmezeitsoll:new Date(2016,03,23,18,00,00), einnahmezeitist:""},
+    {id:"234", mediname:"Paracetamol", medidosis:"100mg", einnahmemenge:"1Tablette", einnahmezeitsoll:new Date(2016,03,12,18,00,00), einnahmezeitist:"2.4.2016 12:05"}];
   
 
-$scope.param = $stateParams.isNichtEingenommen;
+$scope.params = $stateParams.isNichtEingenommen;
+if($scope.params == "true")
+{
+    $scope.nichtGenommen = true;
 
+}else{
+    $scope.nichtGenommen = false;
+}
+
+$scope.updateToggle = function() {
+    if($scope.nichtGenommen == false){
+        $scope.nichtGenommen = true;
+    }else{
+        $scope.nichtGenommen = false;
+    }
+}
+
+$scope.checkFilter = function(x) {
+    if((x.einnahmezeitsoll >= $scope.onezoneDatepickervon.date) && (x.einnahmezeitsoll <= $scope.onezoneDatepickerbis.date)){
+        if($scope.nichtGenommen == true)
+        {
+            if(x.einnahmezeitist == ''){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return true;
+        }
+        
+    }else{
+        return false;
+    }
+    
+}
 
 
       
@@ -41,118 +74,10 @@ $scope.onezoneDatepickervon = {
         
         // your code
 
-
-
-       
-       $scope.von = $scope.onezoneDatepickervon.date.toString().split(" ");
-       $scope.vonTag = $scope.von[2];
-       $scope.vonMonat = $scope.von[1];
-       $scope.vonJahr = $scope.von[3];
-
-       $scope.bis = $scope.onezoneDatepickerbis.date.toString().split(" ");
-       $scope.bisTag = $scope.bis[2];
-       $scope.vonMonat = $scope.von[1];
-       $scope.vonJahr = $scope.von[3];
-       
-       switch($scope.vonMonat){
-           case "Jan":
-                $scope.vonMonat = "1";
-                break;
-           case "Feb":
-                $scope.vonMonat = "2";
-                break;
-           case "Mar":
-                $scope.vonMonat = "3";
-                break;
-           case "Apr":
-                $scope.vonMonat = "4";
-                break;
-           case "May":
-                $scope.vonMonat = "5";
-                break;
-           case "Jun":
-                $scope.vonMonat = "6";
-                break;
-           case "Jul":
-                $scope.vonMonat = "7";
-                break;
-           case "Aug":
-                $scope.vonMonat = "8";
-                break;
-           case "Sep":
-                $scope.vonMonat = "9";
-                break;
-           case "Oct":
-                $scope.vonMonat = "10";
-                break;
-           case "Nov":
-                $scope.vonMonat = "11";
-                break;
-           case "Dec":
-                $scope.vonMonat = "12";
-                break;
+       $scope.showFilter = function(x) {
+           retur(true);
        }
-              switch($scope.bisMonat){
-           case "Jan":
-                $scope.vonMonat = "1";
-                break;
-           case "Feb":
-                $scope.vonMonat = "2";
-                break;
-           case "Mar":
-                $scope.vonMonat = "3";
-                break;
-           case "Apr":
-                $scope.vonMonat = "4";
-                break;
-           case "May":
-                $scope.vonMonat = "5";
-                break;
-           case "Jun":
-                $scope.vonMonat = "6";
-                break;
-           case "Jul":
-                $scope.vonMonat = "7";
-                break;
-           case "Aug":
-                $scope.vonMonat = "8";
-                break;
-           case "Sep":
-                $scope.vonMonat = "9";
-                break;
-           case "Oct":
-                $scope.vonMonat = "10";
-                break;
-           case "Nov":
-                $scope.vonMonat = "11";
-                break;
-           case "Dec":
-                $scope.vonMonat = "12";
-                break;
-       }
-        for(var i = 0; i <$scope.medicine.length; i++)
-        {
-             $scope.einnahmeJahr = $scope.medicine[i].einnahmezeitsoll.substr(4,4);
-             $scope.einnahmeMonat = $scope.medicine[i].einnahmezeitsoll.substr(2,2);
-             $scope.einnahmeTag = $scope.medicine[i].einnahmezeitsoll.substr(0,2);
-           
-            if($scope.einnahmeJahr < $scope.vonJahr || $scope.einnahmeJahr > $scope.bisJahr){
-              $scope.medicine.splice(i,1);  
-            }else if($scope.einnahmeJahr == $scope.vonJahr){
-                if($scope.einnahmeMonat < $scope.vonMonat){
-                    if($scope.einnahmeTag < $scope.vonTag){
-                        $scope.medicine.splice(i,1);
-                    }
-                }
-            }else if($scope.einnahmeJahr == $scope.bisJahr){
-                if($scope.einnameMonat > $scope.bisMonat){
-                    if($scope.einnahmeTag > $scope.bisTag){
-                        $scope.medicine.splice(i,1);
-                    }
-                }
-            }
-        
-    }
+  
     }
 };
 
@@ -176,44 +101,8 @@ $scope.onezoneDatepickerbis = {
     //highlights: highlights,
     callback: function(value){
         // your code
-        $scope.medicine = $scope.medicine1;
-                $scope.vonJahr = $scope.onezoneDatepickervon.date;
-        $scope.vonJahr = $scope.vonJahr.toString().substr(11,4);
-        $scope.vonMonat = $scope.onezoneDatepickervon.date;
-        $scope.vonMonat = $scope.vonMonat.toString().substr(4,2);
-        $scope.vonTag = $scope.onezoneDatepickervon.date;
-        $scope.vonTag = $scope.vonTag.toString().substr(7,2);
         
-        $scope.bisJahr = $scope.onezoneDatepickerbis.date;
-        $scope.bisJahr = $scope.bisJahr.toString().substr(11,4);
-        $scope.bisMonat = $scope.onezoneDatepickerbis.date;
-        $scope.bisMonat = $scope.bisMonat.toString().substr(4,2);
-        $scope.bisTag = $scope.onezoneDatepickerbis.date;
-        $scope.bisTag = $scope.bisTag.toString().substr(7,2);
-        
-        for(var i = 0; i <$scope.medicine.length; i++)
-        {
-             $scope.einnahmeJahr = $scope.medicine[i].einnahmezeitsoll.substr(4,4);
-             $scope.einnahmeMonat = $scope.medicine[i].einnahmezeitsoll.substr(2,2);
-             $scope.einnahmeTag = $scope.medicine[i].einnahmezeitsoll.substr(0,2);
-           
-            if($scope.einnahmeJahr < $scope.vonJahr || $scope.einnahmeJahr > $scope.bisJahr){
-              $scope.medicine.splice(i,1);  
-            }else if($scope.einnahmeJahr == $scope.vonJahr){
-                if($scope.einnahmeMonat < $scope.vonMonat){
-                    if($scope.einnahmeTag < $scope.vonTag){
-                        $scope.medicine.splice(i,1);
-                    }
-                }
-            }else if($scope.einnahmeJahr == $scope.bisJahr){
-                if($scope.einnameMonat > $scope.bisMonat){
-                    if($scope.einnahmeTag > $scope.bisTag){
-                        $scope.medicine.splice(i,1);
-                    }
-                }
-            }
-        
-    }
+
     }
 };
     
