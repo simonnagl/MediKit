7528b754cb8ac0d57a96a18a6018cf3a27e73d18
Simon Nagl <sin6332@thi.de>
2016-05-31 09:57:23 +0200

    Added validation with Cordova dialogs
    Buggy

--- www/js/mediVerwaltungCtrl.js
+++ www/js/mediVerwaltungCtrl.js
@@ -1,6 +1,6 @@
 angular.module('starter.mediVerwaltungCtrl', [])
 
-.controller('mediVerwaltungCtrl', function($scope, $ionicModal, $log, MediStorage) {
+.controller('mediVerwaltungCtrl', function($scope, $ionicModal, $cordovaDialogs, $log, MediStorage) {
     $scope.medis = [];
 
 	$scope.medis = MediStorage.loadAllMedikament();
@@ -13,6 +13,7 @@ angular.module('starter.mediVerwaltungCtrl', [])
             einheit : "ml"
         },
         snoozemin : 15,
+        medibeschreibung: "",
     }
 
     // Create the mediVerwaltung_edit modal that we will use later
@@ -21,18 +22,77 @@ angular.module('starter.mediVerwaltungCtrl', [])
     }).then(function(modal) {
         $scope.medikament = modal;
     });
+    
+    $scope.validateMedi = function(medi) {
+        for(prop in medi) {
+            switch(prop) {
+                case "id":
+                    break;
+                case "mediname":
+                    if(medi[prop] == undefined || medi[prop] == "") {
+                        $cordovaDialogs.alert('Medikamentenname ist leer.', 'Bitte eintragen.', 'OK')
+                        .then(function() {
+                        return false;
+                    });
+                    }
+                    break;
+                case "packungsgroesse":
+                    if(!$scope.validateEinheit(medi.packungsgroesse)) {
+                        return false;
+                    };
+                    break;
+                case "snoozemin":
+                    break;
+                case "medibeschreibung":
+                    break;
+                default:
+                    $log.debug('BUG: Property ' + prop + ': ' + medi[prop] + 'in Medikament.');
+                    break;
+            }
+        }
+        return true;
+    };
+    
+    $scope.validateEinheit = function(groesse) {
+        for(prop in groesse) {
+            switch(prop) {
+                case "menge":
+                    if(groesse[prop] == undefined || groesse[prop] == "") {
+                        $cordovaDialogs.alert('Menge ist leer.', 'Bitte eintragen.', 'OK')
+                        .then(function() {
+                        return false;
+                    });
+                };
+                if(groesse[prop] < '0') {
+                    $cordovaDialogs.alert('Menge ist negativ.', 'Bitte korrigieren.', 'OK')
+                    .then(function() {
+                        return false;
+                    });
+                };
+                break;
+                case "einheit":
+                    break;
+                default:
+                    $log.debug('BUG: Property ' + prop + ': ' + medi[prop] + 'in Menge.');
+                    break;
+            }
+        }
+        return true;
+    };
             
     // Triggered in the medimedikament_neu modal to close it
     $scope.saveMedi = function() {
-        $scope.medis.push($scope.medi);
-		if($scope.isNewMedi) {
-			MediStorage.saveMedikament($scope.medi);
-		} else {
-			MediStorage.updateMedikament($scope.medi);
-		}
+        if(!$scope.validateMedi($scope.medi)) {
+            return false;
+        }
+        if($scope.isNewMedi) {
+            MediStorage.saveMedikament($scope.medi);
+        } else {
+            MediStorage.updateMedikament($scope.medi);
+        }
         $scope.medikament.hide();
         $scope.isNewMedi = undefined;
-		$scope.medis = MediStorage.loadAllMedikament();
+        $scope.medis = MediStorage.loadAllMedikament();
     };
 
     $scope.deleteMedi = function() {
